acd0ca4c1c18c7d2467547a1c0f398c08c040e03
Rao Aaraf Younus <aarafrao22@gmail.com>
2022-06-18 18:07:52 +0500

    v2

--- app/src/main/java/com/aarafrao/charttest/MainActivity.java
+++ app/src/main/java/com/aarafrao/charttest/MainActivity.java
@@ -2,27 +2,38 @@ package com.aarafrao.charttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.IMarker;
 import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.MarkerView;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.CandleEntry;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.IDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.MPPointF;
+import com.github.mikephil.charting.utils.Utils;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<BarEntry> arrayList;
+    BarChart barChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +41,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        BarChart barChart = findViewById(R.id.chart);
+        barChart = findViewById(R.id.chart);
         barChart.setDrawGridBackground(false);
         barChart.setDrawBarShadow(false);
         barChart.setGridBackgroundColor(Color.WHITE);
-        barChart.setTouchEnabled(false);
-        barChart.setPinchZoom(false);
+        barChart.setTouchEnabled(true);
+        barChart.setPinchZoom(true);
+//        barChart.zoom
         barChart.setDoubleTapToZoomEnabled(false);
         barChart.getAxisLeft().setEnabled(false);
         barChart.getAxisRight().setEnabled(false);
@@ -77,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setValueFormatter(new IndexAxisValueFormatter(xVals));
 
-        Typeface tf = Typeface.createFromAsset(getAssets(), "impact.ttf");
+//        Typeface tf = Typeface.createFromAsset(getAssets(), "impact.ttf");
 
 
         LimitLine ll1 = new LimitLine(15f, "Upper Limit");
@@ -86,12 +98,37 @@ public class MainActivity extends AppCompatActivity {
         ll1.enableDashedLine(1.0f, 0.1f, 0f);
         ll1.setLabelPosition(LimitLine.LimitLabelPosition.LEFT_BOTTOM);
         ll1.setTextSize(10f);
-        ll1.setTypeface(tf);
+//        ll1.setTypeface(tf);
 
 
         YAxis leftAxis = barChart.getAxisLeft();
         leftAxis.removeAllLimitLines(); // reset all limit lines to avoid overlapping lines
         leftAxis.addLimitLine(ll1);
+
+//        barChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+//            @Override
+//            public void onValueSelected(Entry e, Highlight h) {
+//                Highlight highlight[] = new Highlight[barChart.getData().getDataSets().size()];
+//                for (int j = 0; j < barChart.getData().getDataSets().size(); j++) {
+//
+//                    IDataSet iDataSet = barChart.getData().getDataSets().get(j);
+//
+//                    for (int i = 0; i < iDataSet.getFormSize(); i++) {
+//                            highlight[j] = new Highlight(e.getX(), e.getY(), j);
+//                    }
+//
+//                }
+//                barChart.highlightValues(highlight);
+//            }
+//
+//            @Override
+//            public void onNothingSelected() {
+//
+//            }
+//        });
+
+        IMarker marker = new MarkerView(this,R.layout.marker);
+        barChart.setMarker(marker);
 
 
     }
@@ -107,5 +144,6 @@ public class MainActivity extends AppCompatActivity {
         arrayList.add(new BarEntry(7, 200));
 
     }
+
 }
 
